5e58846462e73a4567e2660df0d1941e9b96dcc5
ladeason <123998715+ladeason@users.noreply.github.com>
2024-11-21 13:47:22 -0500

    Add files via upload

--- Script/ocean.js
+++ Script/ocean.js
@@ -1,4 +1,14 @@
+let heading = document.getElementById("heading");
+
 let ocean_info = document.getElementById("ocean_info");
+let link = document.getElementById("link");
+
+let toolbar = document.getElementById("toolbar");
+let mode  = document.getElementById("mode");
+let colorblind = document.getElementById("colorblind");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
 
 function changeFontSize(size) {
     // Set value of the parameter as fontSize
@@ -6,17 +16,13 @@ function changeFontSize(size) {
 }
 
 function changeMode() {
-    let toolbar = document.getElementById("toolbar");
-    let mode  = document.getElementById("mode");
-    let button1 = document.getElementById("button1");
-    let button2 = document.getElementById("button2");
-    let button3 = document.getElementById("button3");
-
     toolbar.classList.toggle("toolbar_light");
     mode.classList.toggle("mode_light");
+    colorblind.classList.toggle("colorblind_light");
     button1.classList.toggle("button_light");
     button2.classList.toggle("button_light");
     button3.classList.toggle("button_light");
+
     ocean_info.classList.toggle("ocean_info_light");
 
     if (mode.value === "Light Mode") {
@@ -26,5 +32,28 @@ function changeMode() {
     else {
         mode.value = "Light Mode";
         mode.innerHTML = "Light Mode";
+    }
+}
+
+function toggleColorblind() {
+    heading.classList.toggle("heading_colorblind");
+
+    toolbar.classList.toggle("toolbar_colorblind");
+    mode.classList.toggle("mode_colorblind");
+    colorblind.classList.toggle("colorblind_colorblind");
+    button1.classList.toggle("button_colorblind");
+    button2.classList.toggle("button_colorblind");
+    button3.classList.toggle("button_colorblind");
+
+    ocean_info.classList.toggle("ocean_info_colorblind");
+    link.classList.toggle("link_colorblind");
+
+    if (colorblind.value === "Colorblind Mode ON") {
+        colorblind.value = "Colorblind Mode OFF";
+        colorblind.innerHTML = "Colorblind Mode OFF";
+    }
+    else {
+        colorblind.value = "Colorblind Mode ON";
+        colorblind.innerHTML = "Colorblind Mode ON";
     }
 }
--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,5 @@
+let heading = document.getElementById("heading");
+
 let desert_info = document.getElementById("desert_info");
 let tundra_info = document.getElementById("tundra_info");
 let forest_info = document.getElementById("forest_info");
@@ -10,6 +12,20 @@ let forest = document.getElementById("forest");
 let grassland = document.getElementById("grassland");
 let ocean = document.getElementById("ocean");
 let jungle = document.getElementById("jungle");
+
+let toolbar = document.getElementById("toolbar");
+let mode  = document.getElementById("mode");
+let colorblind = document.getElementById("colorblind");
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+
+let link1 = document.getElementById("link1");
+let link2 = document.getElementById("link2");
+let link3 = document.getElementById("link3");
+let link4 = document.getElementById("link4");
+let link5 = document.getElementById("link5");
+let link6 = document.getElementById("link6");
 
 function changeFontSize(size, space) {
     desert_info.style.fontSize = size;
@@ -27,17 +43,13 @@ function changeFontSize(size, space) {
 }
 
 function changeMode() {
-    let toolbar = document.getElementById("toolbar");
-    let mode  = document.getElementById("mode");
-    let button1 = document.getElementById("button1");
-    let button2 = document.getElementById("button2");
-    let button3 = document.getElementById("button3");
-
     toolbar.classList.toggle("toolbar_light");
     mode.classList.toggle("mode_light");
+    colorblind.classList.toggle("colorblind_light");
     button1.classList.toggle("button_light");
     button2.classList.toggle("button_light");
     button3.classList.toggle("button_light");
+
     desert_info.classList.toggle("desert_info_light");
     tundra_info.classList.toggle("tundra_info_light");
     forest_info.classList.toggle("forest_info_light");
@@ -52,5 +64,39 @@ function changeMode() {
     else {
         mode.value = "Light Mode";
         mode.innerHTML = "Light Mode";
+    }
+}
+
+function toggleColorblind() {
+    heading.classList.toggle("heading_colorblind");
+
+    toolbar.classList.toggle("toolbar_colorblind");
+    mode.classList.toggle("mode_colorblind");
+    colorblind.classList.toggle("colorblind_colorblind");
+    button1.classList.toggle("button_colorblind");
+    button2.classList.toggle("button_colorblind");
+    button3.classList.toggle("button_colorblind");
+
+    desert_info.classList.toggle("desert_info_colorblind");
+    tundra_info.classList.toggle("tundra_info_colorblind");
+    forest_info.classList.toggle("forest_info_colorblind");
+    grassland_info.classList.toggle("grassland_info_colorblind");
+    ocean_info.classList.toggle("ocean_info_colorblind");
+    jungle_info.classList.toggle("jungle_info_colorblind");
+
+    link1.classList.toggle("link_colorblind");
+    link2.classList.toggle("link_colorblind");
+    link3.classList.toggle("link_colorblind");
+    link4.classList.toggle("link_colorblind");
+    link5.classList.toggle("link_colorblind");
+    link6.classList.toggle("link_colorblind");
+
+    if (colorblind.value === "Colorblind Mode ON") {
+        colorblind.value = "Colorblind Mode OFF";
+        colorblind.innerHTML = "Colorblind Mode OFF";
+    }
+    else {
+        colorblind.value = "Colorblind Mode ON";
+        colorblind.innerHTML = "Colorblind Mode ON";
     }
 }
